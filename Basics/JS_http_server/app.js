1e8815e33fa60e3d5b3b24b99f81037ee880d9d1
kavichelvan <41743539+kavichelvan@users.noreply.github.com>
2018-10-08 10:09:15 +0530

    Update app.js

--- Basics/JS_http_server/app.js
+++ Basics/JS_http_server/app.js
@@ -4,11 +4,7 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req,res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-type','text/plain');
-    res.end('Hellow owlrd');
+    res.end('Hellfdafow owlrd');
 });
 
-server.listen(port,hostname,()=>{
-    console.log('Server started on port '+port);
-});
+server.listen(port);
