df97b6adc5344def1967ad513c13c6afbf1f71b6
kavichelvan <41743539+kavichelvan@users.noreply.github.com>
2018-10-08 14:59:38 +0530

    Update app.js

--- Basics/REST_by_JonathanMills/app.js
+++ Basics/REST_by_JonathanMills/app.js
@@ -4,6 +4,25 @@ var express = require('express');
 var app=express();
 var port = process.env.port || 8080;
 
+//creating a router
+var bookRouter =express.Router();
+//router 1
+bookRouter.route('/Books').get(function(req,res){
+    var responseJson = {
+        hello: "This is my api"
+    };
+    res.json(responseJson);
+});
+
+//router 2
+bookRouter.route('/Authors').get(function(req,res){
+    res.send("Authors name");
+});
+
+app.use('/api',bookRouter);
+
+
+//default router
 app.get('/',function(req,res){
     res.send('Welcome to my API with gulp 3');
 });
