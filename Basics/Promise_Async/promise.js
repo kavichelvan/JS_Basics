29e214f57db56a1f1a5827658c6e36340a4b7360
Kavichelvan P <41743539+kavichelvan@users.noreply.github.com>
2018-10-23 11:46:40 +0530

    Update promise.js

--- Basics/Promise_Async/promise.js
+++ Basics/Promise_Async/promise.js
@@ -1,5 +1,7 @@
 var request = require('request');
 
+//https://medium.com/@tkssharma/writing-neat-asynchronous-node-js-code-with-promises-async-await-fa8d8b0bcd7c
+
 var userDetails;
 function initialize() {
 	// Setting URL and headers for request
